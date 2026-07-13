c8acefc34fa0c588ddf250c52fc9cd0a71a3bd16
sebvitaa <seramirezo@udd.cl>
2026-07-13 16:42:25 -0400

    Correo, contraseña y botón no funcional

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -7,14 +7,53 @@ class LoginScreen extends StatefulWidget{
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _correoCtrl=TextEditingController();
+  final _claveCtrl=TextEditingController();
+  bool _verClave = false ;
   @override
+
+  void dispose(){
+    _correoCtrl.dispose();
+    _claveCtrl.dispose();
+    super.dispose();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Iniciar Sesión'),
       ),
       body: Center(
-        child: Text('Login'),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: _correoCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Correo',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _claveCtrl,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                ),
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: () {},
+                  child: const Text('Entrar'),
+                ),
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
